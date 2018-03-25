00dffbf8f7f340e70af03eac273132610858da80
Ryan <ryan.reid@eagles.cui.edu>
2018-03-24 22:10:10 -0700

    updating Star Wars Web Crawl to include the Sith code

--- RasberryPi/FlaskWebServer/app.py
+++ RasberryPi/FlaskWebServer/app.py
@@ -1,18 +1,18 @@
 # Flask file
 
 from flask import Flask, render_template 
-from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return "Hello world"
+	return render_template('index.html')
 
 
-@app.route("/data")
-def data():
-	return render_template("index.html")
+@app.route("/starWars")
+def starWars():
+	return render_template("starWars.html")
+
 
 if __name__ == "__main__":
 	app.run(debug = True, host = "0.0.0.0")
